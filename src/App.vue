e4ee39d26444d5c319891edf82bf92234b43915b
deepakgupta-1 <deepakgupta902765>
2024-09-05 22:39:24 +0530

    login page

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <LogIn/> 
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import LogIn from './components/LogIn.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LogIn
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/LogIn.vue
@@ -0,0 +1,45 @@
+<template>
+
+<div id="logform">
+<form action="#" class="logform">
+    
+<h1>USER LOGIN</h1>
+    <label for="username">USERNAME</label>
+    <input type="text" id="username" name="username" >
+    <label for="password">PASSWORD</label>
+    <input type="password" id="password" name="password" >
+    <button>LOGIN</button>
+    <a href="#">forgot password?</a>
+</form>
+</div>
+
+</template>
+<script>
+export default {
+   name : 'LogIn'
+}
+</script>
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+input,button{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid skyblue;
+}
+button{
+    background-color: skyblue;
+    cursor: pointer;
+}
+
+
+</style>
